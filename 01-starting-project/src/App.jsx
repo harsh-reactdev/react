84b4f07d1e7d93dd87e423de321b1c687195a046
harsh <harsh.reactdev@gmail.com>
2025-08-27 23:41:44 +0530

    chore: setting dynamic attribute values

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,6 @@
+import reactImg from './assets/react-core-concepts.png';
 // Header component
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function getRandomInt(max) {
   const rand = Math.floor(Math.random() * max);
@@ -12,7 +12,7 @@ function getRandomInt(max) {
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {reactDescriptions[getRandomInt(reactDescriptions.length)]} React concepts you will need for almost any app you are
@@ -27,7 +27,7 @@ function Header() {
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
